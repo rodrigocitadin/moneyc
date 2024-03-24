785ef556d299421bc9b9b46e0cf8e89cb268aca9
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-24 14:32:23 -0300

    wip

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
       <div className="w-full max-w-[1100px] mx-auto px-6 flex justify-between items-center">
         <h1 className="font-bold text-4xl">Moneyc</h1>
         <button
-          className="h-12 border-none bg-blue-500 text-white font-bold px-4 rounded-md hover:bg-blue-700  transition-colors duration-200"
+          className="h-12 border-none bg-blue-500 text-white font-bold px-4 rounded-md hover:bg-blue-700 transition-colors duration-200"
         >
           New transaction
         </button>
