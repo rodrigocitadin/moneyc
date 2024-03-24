e3e096b19a312817e8e4d7a5a72a436782b68bb8
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-24 15:09:23 -0300

    finish modal style

--- src/components/NewTransactionModal.tsx
+++ src/components/NewTransactionModal.tsx
@@ -1,23 +1,46 @@
 import { Close, Content, Overlay, Portal, Title } from "@radix-ui/react-dialog";
+import { X } from "phosphor-react";
 
 export default function NewTransactionModal() {
   return (
     <Portal>
-      <Overlay />
-      <Content>
-        <Title>New transaction</Title>
+      <Overlay className="fixed h-screen w-screen inset-0 bg-[#00000075]" />
+      <Content className="min-w-[32rem] rounded-md p-12 bg-gray-800 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
 
-        <form>
-          <input type="text" placeholder="Description" required/>
-          <input type="number" placeholder="Amount" required/>
-          <input type="text" placeholder="Category" required/>
+        <div className="flex justify-between items-center">
+          <Title className="text-3xl font-bold">New transaction</Title>
+          <Close className="bg-transparent text-gray-500 block leading-none">
+            <X size={24} />
+          </Close>
+        </div>
 
-          <button>
+
+        <form className="flex mt-8 flex-col gap-4">
+          <input
+            className="rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500"
+            type="text"
+            placeholder="Description"
+            required
+          />
+          <input
+            className="rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500"
+            type="number"
+            placeholder="Amount"
+            required
+          />
+          <input
+            className="rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500"
+            type="text"
+            placeholder="Category"
+            required
+          />
+
+          <button
+            className="h-16 border-none bg-blue-500 hover:bg-blue-700 transition-colors duration-200 text-white font-bold rounded-md mt-6"
+          >
             Submit
           </button>
         </form>
-
-        <Close />
       </Content>
     </Portal>
   )
