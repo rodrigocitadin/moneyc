2a6ef52692552a74ef38a80517e5df6d29189968
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-26 17:08:19 -0300

    wip

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,28 +1,48 @@
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
+import { useContext } from "react";
+import { TransactionsContext } from "../contexts/TransactionsContext";
 
 export default function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.kind === 'income') {
+      acc.income += transaction.price
+      acc.total += transaction.price
+    }
+
+    if (transaction.kind === 'outcome') {
+      acc.outcome += transaction.price
+      acc.total -= transaction.price
+    }
+
+    return acc;
+  }, {
+    income: 0, outcome: 0, total: 0
+  });
+
   return (
     <div className="w-full max-w-[1100px] mx-auto px-6 grid grid-cols-3 gap-8 mt-[-5rem]">
       <div className="bg-gray-600 rounded-md p-8">
         <header className="flex items-center justify-between text-gray-300">
           <span>Income</span>
-          <ArrowCircleUp size={32} className="text-blue-500"/>
+          <ArrowCircleUp size={32} className="text-blue-500" />
         </header>
-        <strong className="block mt-4 text-3xl">USD 17,450.50</strong>
+        <strong className="block mt-4 text-3xl">USD {summary.income}</strong>
       </div>
       <div className="bg-gray-600 rounded-md p-8">
         <header className="flex items-center justify-between text-gray-300">
           <span>Outcome</span>
-          <ArrowCircleDown size={32} className="text-red-500"/>
+          <ArrowCircleDown size={32} className="text-red-500" />
         </header>
-        <strong className="block mt-4 text-3xl">USD 13,200.70</strong>
+        <strong className="block mt-4 text-3xl">USD {summary.outcome}</strong>
       </div>
       <div className="bg-blue-700 rounded-md p-8">
         <header className="flex items-center justify-between text-gray-300">
           <span>Total</span>
-          <CurrencyDollar size={32} className="text-white"/>
+          <CurrencyDollar size={32} className="text-white" />
         </header>
-        <strong className="block mt-4 text-3xl">USD 4,249.80</strong>
+        <strong className="block mt-4 text-3xl">USD {summary.total}</strong>
       </div>
     </div>
   )
