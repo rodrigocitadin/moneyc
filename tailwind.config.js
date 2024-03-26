2dbfadac645dee063fce010345ddb4d5eafe7da1
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-26 20:45:27 -0300

    new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,9 +19,12 @@ export default {
       'red-300': '#F75A68',
       'red-500': '#AB222E',
       'red-700': '#7A1921',
-      'blue-300': '#00B0B3',
-      'blue-500': '#008587',
-      'blue-700': '#015D5F'
+      'green-300': '#5AF75F',
+      'green-500': '#22AB27',
+      'green-700': '#197A1C',
+      'purple-300': '#9B5AF7',
+      'purple-500': '#5B22AB',
+      'purple-700': '#41197A',
     }
   },
   plugins: [],
