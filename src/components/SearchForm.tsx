d36accc5a4b9cbf0c43a622f48c77109cf9ff9f4
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-26 21:31:28 -0300

    wip in forms

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,12 +1,30 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { MagnifyingGlass } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import * as z from 'zod';
+
+const searchFormSchema = z.object({
+  query: z.string()
+});
+
+type SearchFormInput = z.infer<typeof searchFormSchema>;
 
 export default function SearchForm() {
+  const { register, handleSubmit } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchFormSchema)
+  })
+
+  function handleFormSubmit(data: SearchFormInput) {
+    console.log(data);
+  }
+
   return (
-    <form className="flex gap-4">
+    <form onSubmit={handleSubmit(handleFormSubmit)}  className="flex gap-4">
       <input
         className="flex-1 rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500"
         placeholder="Search for a transaction"
         type="text"
+        {...register('query')}
       />
       <button
         className="transition-all duration-200 flex items-center gap-4 p-4 bg-transparent border-2 border-purple-300 text-purple-300 font-bold rounded-md hover:bg-purple-500 hover:border-purple-500 hover:text-white"
