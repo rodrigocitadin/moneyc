88e6b41d6c74809fdfe9b0674e506c3a14547ade
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-28 12:18:04 -0300

    wip

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -24,17 +24,19 @@ export const TransactionsContext = createContext({} as TransactionsContextType);
 export function TransactionsProvider({ children }: { children: React.ReactNode }) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function fetchTransactions(query?: string) {
-    const res = await api.get("transactions", {
-      params: {
-        _sort: "createdAt",
-        _order: "desc",
-        q: query
-      }
-    });
+  const fetchTransactions = useCallback(
+    async (query?: string) => {
+      const res = await api.get("transactions", {
+        params: {
+          _sort: "createdAt",
+          _order: "desc",
+          q: query
+        }
+      });
 
-    setTransactions(res.data);
-  }
+      setTransactions(res.data);
+    }, []
+  )
 
   const createTransaction = useCallback(async (data: CreateTransactionData) => {
     if (data.kind === "outcome") data.price = data.price * -1;
