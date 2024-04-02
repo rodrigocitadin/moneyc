f339044dce10b33b96e1e0132c69afaea2f1930f
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-02 12:51:21 -0300

    update modal colors

--- src/components/NewTransactionModal.tsx
+++ src/components/NewTransactionModal.tsx
@@ -35,11 +35,11 @@ export default function NewTransactionModal() {
   return (
     <Portal>
       <Overlay className="fixed h-screen w-screen inset-0 bg-[#00000075]" />
-      <Content className="md:min-w[32rem] max-w-[32rem] rounded-md p-12 bg-gray-800 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Content className="md:min-w[32rem] max-w-[32rem] rounded-md p-12 bg-white fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
 
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-start md:items-center">
           <Title className="text-3xl font-bold">New transaction</Title>
-          <Close className="bg-transparent text-gray-500 block leading-none">
+          <Close className="bg-transparent block leading-none">
             <X size={24} />
           </Close>
         </div>
@@ -47,21 +47,21 @@ export default function NewTransactionModal() {
 
         <form onSubmit={handleSubmit(handleNewFormSubmit)} className="flex mt-8 flex-col gap-4">
           <input
-            className="rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500"
+            className="border-b-2 border-b-gray-400 text-gray-800 p-4 placeholder:text-gray-500"
             type="text"
             placeholder="Description"
             required
             {...register("description")}
           />
           <input
-            className="rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500"
+            className="border-b-2 border-b-gray-400 text-gray-800 p-4 placeholder:text-gray-500"
             type="number"
             placeholder="Amount"
             required
             {...register("price", { valueAsNumber: true })}
           />
           <input
-            className="rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500"
+            className="border-b-2 border-b-gray-400 text-gray-800 p-4 placeholder:text-gray-500"
             type="text"
             placeholder="Category"
             required
@@ -74,12 +74,12 @@ export default function NewTransactionModal() {
             render={({ field }) => {
               return (
                 <Root onValueChange={field.onChange} value={field.value} className="grid grid-cols-2 gap-4 mt-2">
-                  <Item value="income" className="data-[state=unchecked]:hover:bg-gray-600 *:data-[state=checked]:text-white data-[state=checked]:text-white data-[state=checked]:bg-green-500 bg-gray-700 p-4 flex items-center justify-center gap-2 rounded-md text-gray-300">
-                    <ArrowCircleUp size={24} className="text-green-500" />
+                  <Item value="income" className="data-[state=unchecked]:hover:border-green-500 data-[state=unchecked]:hover:text-green-500 data-[state=checked]:border-green-500 *:data-[state=checked]:text-white data-[state=checked]:text-white data-[state=checked]:bg-green-500 bg-white p-4 flex items-center justify-center gap-2 rounded-md border-2 border-black">
+                    <ArrowCircleUp size={24} />
                     <span className="hidden sm:inline">Income</span>
                   </Item>
-                  <Item value="outcome" className="data-[state=unchecked]:hover:bg-gray-600 *:data-[state=checked]:text-white data-[state=checked]:text-white data-[state=checked]:bg-red-500 bg-gray-700 p-4 flex items-center justify-center gap-2 rounded-md text-gray-300">
-                    <ArrowCircleDown size={24} className="text-red-500" />
+                  <Item value="outcome" className="data-[state=unchecked]:hover:border-red-500 data-[state=unchecked]:hover:text-red-500 data-[state=checked]:border-red-500 *:data-[state=checked]:text-white data-[state=checked]:text-white data-[state=checked]:bg-red-500 bg-white p-4 flex items-center justify-center gap-2 rounded-md border-2 border-black">
+                    <ArrowCircleDown size={24} />
                     <span className="hidden md:inline">Outcome</span>
                   </Item>
                 </Root>
@@ -89,7 +89,7 @@ export default function NewTransactionModal() {
 
 
           <button
-            className="h-16 border-none bg-purple-500 [&:not(:disabled)]:hover:bg-purple-700 transition-colors duration-200 text-white font-bold rounded-md mt-6"
+            className="h-16 border-none bg-black [&:not(:disabled)]:hover:bg-gray-800 transition-colors duration-200 text-white font-bold rounded-md mt-6"
             disabled={isSubmitting}
           >
             Submit
