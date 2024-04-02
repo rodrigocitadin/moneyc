fd03bc8ecadfa126e8f910b86fa9e08d6705f1af
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-02 13:05:15 -0300

    update summary colors

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -7,24 +7,24 @@ export default function Summary() {
 
   return (
     <div className="md:grid w-full max-w-[1100px] mx-auto px-6 flex flex-col md:grid-cols-3 gap-8 mt-[-5rem]">
-      <div className="bg-gray-600 rounded-md p-8">
-        <header className="flex items-center justify-between text-gray-300">
+      <div className="bg-black rounded-md p-8">
+        <header className="flex items-center justify-between">
           <span>Income</span>
-          <ArrowCircleUp size={32} className="text-green-500" />
+          <ArrowCircleUp size={32} className="text-green-300" />
         </header>
-        <strong className="block mt-4 text-3xl">{currencyFormatter.format(summary.income)}</strong>
+        <strong className="text-green-300 block mt-4 text-3xl">{currencyFormatter.format(summary.income)}</strong>
       </div>
-      <div className="bg-gray-600 rounded-md p-8">
-        <header className="flex items-center justify-between text-gray-300">
+      <div className="bg-black rounded-md p-8">
+        <header className="flex items-center justify-between">
           <span>Outcome</span>
-          <ArrowCircleDown size={32} className="text-red-500" />
+          <ArrowCircleDown size={32} className="text-red-300" />
         </header>
-        <strong className="block mt-4 text-3xl">{currencyFormatter.format(summary.outcome)}</strong>
+        <strong className="text-red-300 block mt-4 text-3xl">{currencyFormatter.format(summary.outcome)}</strong>
       </div>
-      <div className="bg-purple-700 rounded-md p-8">
-        <header className="flex items-center justify-between text-gray-300">
+      <div className="bg-black rounded-md p-8">
+        <header className="flex items-center justify-between">
           <span>Total</span>
-          <CurrencyDollar size={32} className="text-white" />
+          <CurrencyDollar size={32} />
         </header>
         <strong className="block mt-4 text-3xl">{currencyFormatter.format(summary.total)}</strong>
       </div>
