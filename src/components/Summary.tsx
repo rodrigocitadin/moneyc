6e6349e3b4cdc6ca21f99b46cdc74c1a3879cf77
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-02 13:13:26 -0300

    fix again

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -9,24 +9,24 @@ export default function Summary() {
     <div className="md:grid w-full max-w-[1100px] mx-auto px-6 flex flex-col md:grid-cols-3 gap-8 mt-[-5rem]">
       <div className="bg-black rounded-md p-8">
         <header className="flex items-center justify-between">
-          <span>Income</span>
+          <span className="text-white">Income</span>
           <ArrowCircleUp size={32} className="text-green-300" />
         </header>
         <strong className="text-green-300 block mt-4 text-3xl">{currencyFormatter.format(summary.income)}</strong>
       </div>
       <div className="bg-black rounded-md p-8">
         <header className="flex items-center justify-between">
-          <span>Outcome</span>
+          <span className="text-white">Outcome</span>
           <ArrowCircleDown size={32} className="text-red-300" />
         </header>
         <strong className="text-red-300 block mt-4 text-3xl">{currencyFormatter.format(summary.outcome)}</strong>
       </div>
       <div className="bg-black rounded-md p-8">
         <header className="flex items-center justify-between">
-          <span>Total</span>
+          <span className="text-white">Total</span>
           <CurrencyDollar size={32} />
         </header>
-        <strong className="block mt-4 text-3xl">{currencyFormatter.format(summary.total)}</strong>
+        <strong className="text-gray-200 block mt-4 text-3xl">{currencyFormatter.format(summary.total)}</strong>
       </div>
     </div>
   );
